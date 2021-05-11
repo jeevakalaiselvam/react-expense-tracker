76852414327de16716319617f7ad10184cd7a6eb
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-05-11 20:39:31 +0530

    Sample Data working

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
     const expenses = [
-        { id: 1, name: "Expense 1", date: new Date() },
-        { id: 2, name: "Expense 2", date: new Date() },
+        { id: 1, name: "Expense 1", date: new Date(), amount: 200 },
+        { id: 2, name: "Expense 2", date: new Date(), amount: 150 },
     ];
 
     return (
         <div className="App">
+            <h1>Expense Tracker</h1>
             {expenses.map((expense) => {
                 return (
                     <ExpenseItem
                         name={expense.name}
                         key={expense.id}
                         date={expense.date}
+                        amount={expense.amount}
                     ></ExpenseItem>
                 );
             })}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,23 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem({ name }) {
-    return <div className="expense">{name}</div>;
+function ExpenseItem(props) {
+    const name = props.name;
+    const amount = props.amount;
+    const month = props.date.toLocaleString("en-US", { month: "long" });
+    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+    const year = props.date.getFullYear();
+
+    return (
+        <div className="expense">
+            <div className="date">
+                <div className="date-data">{month}</div>
+                <div className="date-data">{day}</div>
+                <div className="date-data">{year}</div>
+            </div>
+            <h2 className="title">{name}</h2>
+            <h2 className="amount">${amount}</h2>
+        </div>
+    );
 }
 
 export default ExpenseItem;
